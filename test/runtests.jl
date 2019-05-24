c1e48778f80ff29eb52cfc7a66b1c816ecf42dee
Anshul Singhvi <anshulsinghvi@gmail.com>
2019-05-24 06:21:06 +0530

    add testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,27 @@
+using Test
 using BibTeX
 
-import Documenter
-Documenter.makedocs(
-    modules = [BibTeX],
-    format = Documenter.html(),
-    sitename = "BibTeX.jl",
-    root = joinpath(dirname(dirname(@__FILE__)), "docs"),
-    pages = Any["Home" => "index.md"],
-    strict = true,
-    linkcheck = true,
-    checkdocs = :exports,
-    authors = "Brandon Taylor"
-)
+@testset "Parsing invalid specs" begin
+    
+    @test_throws ErrorException parse_bibtex("@book")
+    
+    @test_throws ErrorException parse_bibtex("@book@")
+    
+    
+end
 
-# just test if it parses (for now)
-joinpath((@__FILE__) |> dirname |> dirname, "example", "examples.bib") |> f -> read(f, string) |> parse_bibtex
+@testset "Parsing a file" begin
+    # just test if it parses (for now)
+    @test_nowarn preamble, result = joinpath((@__FILE__) |> dirname |> dirname, "example", "examples.bib") |> f -> read(f, string) |> parse_bibtex
+    
+    @test preamble == "some instructions"
+    
+    @test result["b"]["type"] == "a"
+    
+    @test result["b"]["c"] == "{ c } c"
+    
+    @test result["b"]["d"] == "d d"
+    
+    @test result["b"]["e"] == "f short"
+    
+end
